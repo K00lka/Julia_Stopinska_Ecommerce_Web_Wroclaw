ffd2d2d251eaeb3bf20e8cd794d5e32a15730d99
K00lka <sali60321@gmail.com>
2025-05-11 22:08:37 +0200

    change vite config for gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build:{
+    outDir: 'build',
+  },
   plugins: [
     tailwindcss(),
   ],
